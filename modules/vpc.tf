ac3dde930330c351875f49912303eccddab5a8fc
Shreyas Vijay Purkar <purkar.s@northeastern.edu>
2025-02-18 18:43:02 -0500

    Add code validation issue

--- modules/vpc.tf
+++ modules/vpc.tf
@@ -1,6 +1,6 @@
 # Create a VPC
 resource "aws_vpc" "csye6225_vpc" {
-  cidr_block           = var.vpc_cidr
+  cidr_block           = "256.0.0.0/16"
   instance_tenancy     = var.instance_tenancy
   enable_dns_support   = true
   enable_dns_hostnames = true
