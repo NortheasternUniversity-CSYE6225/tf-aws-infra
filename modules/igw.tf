e05facf51871dc3782375dad38157ff2154ac45f
Shreyas Vijay Purkar <purkar.s@northeastern.edu>
2025-02-19 11:41:28 -0500

    Add fmt and validation error

--- modules/igw.tf
+++ modules/igw.tf
@@ -1,6 +1,6 @@
 # Create a Internet Gateway
 resource "aws_internet_gateway" "csye6225_igw" {
-  vpc_id = aws_vpc.csye6225_vpc
+  vpc_id = aws_vpc.csye6225_vpc.id
 
   tags = {
     Name = "${var.environment}-csye6225-igw"
